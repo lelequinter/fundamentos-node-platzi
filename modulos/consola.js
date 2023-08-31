0bca53c323a9f706ef53aa7bcdf8e83bba91fc5e
lelequinter <lele.quinter@gmail.com>
2023-08-31 14:27:18 -0500

    console

---
+++ modulos/consola.js
@@ -0,0 +1,50 @@
+// console.log('Algo');
+// console.info('Algo');
+// console.warn('Algo');
+// console.error('Algo');
+
+// var tabla = [
+//     {
+//         a: 1,
+//         b: 'z'
+//     },
+//     {
+//         a: 2,
+//         b: 'x'
+//     },
+// ];
+
+// console.log(tabla);
+// console.table(tabla);
+
+// console.group('Conversación');
+// console.log('Hola');
+// console.log('Bla bla bla');
+// console.log('Adiós');
+// console.groupEnd('Conversación');
+
+// console.log('Log de otra cosa');
+
+// function function1() {
+//     console.group('function1');
+//     console.log('Esto es de la funcion1');
+//     console.log('Esto tambien');
+//     function2();
+//     console.log('De veunta a funcion1');
+//     console.groupEnd('function1');
+// }
+
+// function function2() {
+//     console.group('funcion2');
+//     console.log('Ahora estamos en la funcion 2');
+//     console.groupEnd('funcion2');
+// }
+
+// console.log('Empezamos');
+// function1();
+
+// console.count('veces');
+// console.count('veces');
+// console.count('veces');
+// console.countReset('veces');
+// console.count('veces');
